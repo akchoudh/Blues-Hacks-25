df3d6816633dafdee75ccee4e16d34c2a42dfedc
akchoudh <choak@utschools.ca>
2025-02-01 13:04:59 -0500

    Update FRED_Data.py

--- FRED_Data.py
+++ FRED_Data.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 #Our FRED API key
-api_key = "6b6db579163639914284f1e5729cc668"
+api_key = "***********"
 fred = Fred(api_key)  #Initializse Fred with our API key
 
 class giveData:
