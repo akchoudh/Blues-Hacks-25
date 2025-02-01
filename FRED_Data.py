be9f6c319a7252ab4f875c6cad22cb385d35af44
akchoudh <choak@utschools.ca>
2025-02-01 16:48:02 -0500

    Update FRED_Data.py

--- FRED_Data.py
+++ FRED_Data.py
@@ -1,55 +1,52 @@
 from fredapi import Fred
 import pandas as pd
 import matplotlib.pyplot as plt
+from flask import Flask, render_template
 
 #Our FRED API key
-api_key = "********"
-fred = Fred(api_key)  #Initializse Fred with our API key
+api_key = "6b6db579163639914284f1e5729cc668"
+fred = Fred(api_key)  #
 
 class giveData:
 
     @staticmethod
     def exchange_rate(start_date, end_date):
-        #Fetches the exchange rate using the 'DEXCAUS' series
         exchange_rate_data = fred.get_series('DEXCAUS', start_date, end_date)
         return exchange_rate_data
 
 
     @staticmethod
     def unemployment_rate(start_date, end_date):
-        #Fetches Canada's unemployment rate
         unemployment_data = fred.get_series("LRUNTTTTCAM156S", start_date, end_date)
         return unemployment_data
 
     @staticmethod
     def cpi(start_date, end_date):
-        #Fetches consumer price index
+        #Consumer price index
         cpi_data = fred.get_series("CPALTT01CAM659N", start_date, end_date)
         return cpi_data
 
 
     @staticmethod
     def housing_cost(start_date, end_date):
-        #Fetches housing as a percent of 2010 prices
+        #Housing as a % of 2010 prices
         housing_cost_data = fred.get_series("QCAN628BIS", start_date, end_date)
         return housing_cost_data
 
     @staticmethod
     def interest_rate(start_date, end_date):
-        # Fetches BOC interest rate
         interest_rate_data = fred.get_series("IRLTLT01CAM156N", start_date, end_date)
         return interest_rate_data
 
     @staticmethod
     def export_value(start_date, end_date):
-        #Fetches %of sales that are exports
+        #%of sales that are exports
         export_value_data = fred.get_series("XTEXVA01CAQ188S", start_date, end_date)
         return export_value_data
 
 
 class graphData:
     data = giveData()
-
     def graph(self, func, start_date, end_date):
 
         #Fetches the data from function *func*
@@ -57,16 +54,20 @@ class graphData:
 
         #Avoids an error
         if series_data.empty:
-            print("No data available for the specified time period.")
+            print("Data is unavaliable for this time period.")
             return
 
-        # Plotting the series
+        #Plots the series
         plt.figure(figsize=(10, 5))
         series_data.plot()
         plt.title(f"{func.__name__.replace('_', ' ').capitalize()} from {start_date} to {end_date}")
         plt.xlabel("Date")
         plt.ylabel(func.__name__.replace('_', ' ').capitalize())
-        plt.grid(True)
-        plt.show()
-graph = graphData()
-graph.graph(graph.data.exchange_rate, "2022-01-01", "2023-01-01")
+        plt_path = "static/plot.png"
+        plt.savefig(plt_path)
+        plt.close()
+        return plt_path
+
+
+
+
