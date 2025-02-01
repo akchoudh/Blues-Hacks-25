e0b77c400bdb7475f6221f849b21bb07ce95327c
akchoudh <choak@utschools.ca>
2025-02-01 16:48:24 -0500

    Update FRED_Data.py

--- FRED_Data.py
+++ FRED_Data.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from flask import Flask, render_template
 
 #Our FRED API key
-api_key = "6b6db579163639914284f1e5729cc668"
+api_key = "*****"
 fred = Fred(api_key)  #
 
 class giveData:
